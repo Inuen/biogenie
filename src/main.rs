371297b59fa1ae097c0d1ac2f6dd34063df07ff1
Michał Nguyen <michalnguyen99@gmail.com>
2025-03-12 23:26:04 +0100

    Test and bench 3 algorithms for text searching

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,14 @@
 #[macro_use]
 extern crate timeit;
+use aho_corasick::AhoCorasick;
+use bio::pattern_matching::bom::BOM;
+use memchr::memmem;
 use nucgen::Sequence;
 use rayon::prelude::*;
 use rustc_hash::{FxBuildHasher, FxHashMap};
+use std::fs;
 use std::io::Read;
-use std::str::from_utf8;
-use std::thread::{Scope, scope};
-use std::{env, fs};
 use timeit::timeit;
-
-pub fn run_parallel_on_thread_pool<'env, F, T>(f: F) -> T
-where
-    F: for<'scope> FnOnce(&'scope Scope<'scope, 'env>) -> T,
-{
-    todo!();
-}
-
 
 fn generate_data(size: usize) -> Sequence {
     let mut rng = rand::thread_rng();
@@ -25,90 +18,128 @@ fn generate_data(size: usize) -> Sequence {
     genome
 }
 
+fn remove_exact_using_starts_with(
+    mut sequence: &[u8],
+    adapter: &[u8],
+    allow_multiple: &bool,
+) -> Vec<u8> {
+    let mut trimmed: Vec<u8> = Vec::with_capacity(sequence.len());
+    while sequence.starts_with(adapter) {
+        sequence = &sequence[adapter.len()..];
+        if !allow_multiple {
+            break;
+        }
+    }
+    trimmed.extend_from_slice(sequence);
+    trimmed
+}
+
+fn remove_exact_using_ends_with(
+    mut sequence: &[u8],
+    adapter: &[u8],
+    allow_multiple: &bool,
+) -> Vec<u8> {
+    let mut trimmed: Vec<u8> = Vec::with_capacity(sequence.len());
+    while sequence.ends_with(adapter) {
+        sequence = &sequence[..sequence.len() - adapter.len()];
+        if !allow_multiple {
+            break;
+        }
+    }
+    trimmed.extend_from_slice(sequence);
+    trimmed
+}
+
+fn remove_exact_using_corasick(sequence: &[u8], adapter: &[u8], allow_multiple: &bool) -> Vec<u8> {
+    let mut trimmed = Vec::with_capacity(sequence.len());
+    let ac = AhoCorasick::new([adapter]).unwrap();
+    let mut last_removed_idx: usize = 0;
+    
+    for matched in ac.find_iter(sequence) {
+        trimmed.extend_from_slice(&sequence[last_removed_idx..matched.start()]);
+        last_removed_idx = matched.end();
+        if !allow_multiple {
+            break;
+        }
+    }
+    trimmed.extend_from_slice(&sequence[last_removed_idx..]);
+    trimmed
+}
+
+fn remove_using_bom(sequence: &[u8], adapter: &[u8], allow_multiple: &bool) -> Vec<u8> {
+    let bom = BOM::new(adapter);
+    let occ: Vec<usize> = bom.find_all(sequence).collect();
+    let mut trimmed = Vec::with_capacity(sequence.len());
+
+    let mut last_idx = 0;
+    for match_idx in occ {
+        if match_idx < last_idx {
+            continue;
+        }
+        trimmed.extend_from_slice(&sequence[last_idx..match_idx]);
+        last_idx = match_idx + adapter.len();
+        if !allow_multiple {
+            break;
+        }
+    }
+    trimmed
+}
+
+fn remove_exact_using_memchcr(sequence: &[u8], adapter: &[u8], allow_multiple: &bool) -> Vec<u8> {
+    let mut trimmed = Vec::with_capacity(sequence.len());
+
+    let mut read_idx = 0;
+
+    while let Some(idx) = memmem::find(&sequence[read_idx..], adapter) {
+        let adapter_idx = idx + read_idx;
+
+        trimmed.extend_from_slice(&sequence[read_idx..adapter_idx]);
+        read_idx = adapter_idx + adapter.len();
+        if !allow_multiple {
+            break;
+        }
+    }
+    trimmed.extend_from_slice(&sequence[read_idx..]);
+    trimmed
+}
+
 #[derive(Clone, Debug)]
-enum TrimModes {
-    RemoveFromStart,
-    RemoveFromEnd,
-    RemoveFromAnywhere,
+enum TrimmingModes {
+    AtStartOnly,
+    AtStartWithPrefix,
+    AtEndWithPrefix,
+    AtEndOnly,
+    Anywhere,
 }
 
 #[derive(Clone)]
 enum Tasks {
     CountKMers(usize),
-    FastAdapt {
-        mode: TrimModes,
-        sequence: &'static str,
+    TrimAdapt {
+        mode: TrimmingModes,
+        sequence: &'static [u8],
+        adapter: &'static [u8],
+        allow_multiple: bool,
+        fuzzy_matching: bool,
     },
 }
 
-#[derive(Clone)]
-struct ParallelTask {
-    input_file_path: &'static str,
-    output_file_path: &'static str,
-    task: Tasks,
-}
-
-impl ParallelTask {
-    fn new(input_file_path: &'static str, output_file_path: &'static str, task: Tasks) -> Self {
-        Self {
-            input_file_path,
-            output_file_path,
-            task,
-        }
-    }
-
-    fn run(&self) {
-        match &self.task {
-            Tasks::CountKMers(i) => {
-                let local_contents = fs::read_to_string(self.input_file_path)
-                    .expect("Should have been able to read the file");
-                let estimated_count: usize = local_contents.len() - (i + 1);
-                let mut ot = FxHashMap::with_capacity_and_hasher(estimated_count, FxBuildHasher);
-                local_contents.as_bytes().windows(*i).for_each(|window| {
-                    ot.entry(window).and_modify(|cnt| *cnt += 1).or_insert(1);
-                });
-            }
-            Tasks::FastAdapt { mode, sequence } => (),
-        };
-    }
-}
 
 fn main() {
-    let kmer_size: usize = 3;
-    let pt = ParallelTask::new(
-        "data/inputs/sequence1_10_000_000.txt",
-        "data/outputs/output1.txt",
-        Tasks::CountKMers(kmer_size),
-    );
-    let tasks = [pt.clone(), pt.clone(), pt.clone(), pt.clone()];
-    let contents = fs::read_to_string(
-        "data/inputs/sequence1_10_000_000.txt",
-    )
-    .expect("Should have been able to read the file");
+    let contents = fs::read_to_string("data/inputs/sequence_100_000_000.txt")
+        .expect("Should have been able to read the file");
+    let adapter = "AAA";
 
+    println!("memmem");
     timeit!({
-        let pool = rayon::ThreadPoolBuilder::new()
-            // for .num_threads() we use default i.e. by number of available CPUs
-            .build()
-            .unwrap();
-        pool.install(|| {
-            scope(|s| {
-                let local_tasks = tasks.clone();
-                for task in local_tasks {
-                    s.spawn(move || task.run());
-                }
-            })
-        })
+        remove_exact_using_memchcr(contents.as_bytes(), adapter.as_bytes(), &true);
     });
-    println!("/////////////");
-
+    println!("corasick");
     timeit!({
-        let estimated_count: usize = contents.len() - kmer_size + 1;
-        for _ in 0..4 {
-            let mut ot = FxHashMap::with_capacity_and_hasher(estimated_count, FxBuildHasher);
-            contents.as_bytes().windows(3).for_each(|window| {
-                ot.entry(window).and_modify(|cnt| *cnt += 1).or_insert(1);
-            });
-        }
+        remove_exact_using_corasick(contents.as_bytes(), adapter.as_bytes(), &true);
+    });
+    println!("bom");
+    timeit!({
+        remove_using_bom(contents.as_bytes(), adapter.as_bytes(), &true);
     });
 }
